ca6f37b7ac7939d92c2f1013192055c31dd53756
Bilgihan Kose <bilgihankose@gmail.com>
2022-02-02 13:16:03 +0300

    added searchbar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input @input="onInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput(event){
+      //emitting events to the app component
+      this.$emit('termChange', event.target.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
